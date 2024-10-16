1d59c0dc4c25fd8eb7bb7a4af5fe256197bcbf1e
Maffittaa <mafaldamartins2001@gmail.com>
2024-10-16 13:08:31 +0100

    Movement done, starting consumables

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    [SerializeField] private int VelocityModifier = 4;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        myRigidbody.velocity = new Vector2
+            (
+                Input.GetAxis("Horizontal") * VelocityModifier,
+                Input.GetAxis("Vertical") * VelocityModifier
+            );
+    }
+}
