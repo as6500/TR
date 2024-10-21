605fe521b4fc5edb815b4464f633fed6489af21a
Maffittaa <mafaldamartins2001@gmail.com>
2024-10-21 18:03:48 +0100

    timer for flasks done, trying flasks again, missing 2hp per  second

--- Assets/Scripts/UI/Comsumables/AntiRadiationFlaskUIScript.cs
+++ Assets/Scripts/UI/Comsumables/AntiRadiationFlaskUIScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AntiRadiationFlaskUIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private int numberOfFlasks = 5;
+	private AntiRadiationScript antiRadiationScript;
+	private HealthScript healthScript;
+	private void Start()
+	{
+		antiRadiationScript = GameObject.FindGameObjectWithTag("Player").GetComponent<AntiRadiationScript>();
+		healthScript = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthScript>();
+		gameObject.GetComponent<Text>().text = numberOfFlasks.ToString();
+	}
+	public void FlasksCount()
+	{
+		if (numberOfFlasks > 0)
+		{
+			numberOfFlasks--;
+			gameObject.GetComponent<Text>().text = numberOfFlasks.ToString();
+		}
+	}
 }
---
+++ Assets/Scripts/UI/Timer/AntiRadiationTimer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AntiRadiationTimer : MonoBehaviour
+{
+	private float timeRemaining = 10;
+	private bool isTimerOn = false;
+	private Text timerText;
+
+	public void Start()
+	{
+		isTimerOn = true;
+	}
+
+	public void Update()
+	{
+		if (isTimerOn == true)
+		{
+			if (timeRemaining > 0)
+			{
+				timeRemaining -= Time.deltaTime;
+				timeDisplayed(timeRemaining);
+			}
+			else
+			{
+				timeRemaining = 0;
+				isTimerOn= false;
+			}
+		}
+	}
+
+	public void timeDisplayed(float timeOnScreen)
+	{
+		float minutes = Mathf.FloorToInt(timeOnScreen / 60); //calculate the minutes
+		float seconds = Mathf.FloorToInt(timeOnScreen % 60); //calculate the seconds
+
+		timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+	}
+}
