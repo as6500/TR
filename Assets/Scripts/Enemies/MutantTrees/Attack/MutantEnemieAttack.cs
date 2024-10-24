cd775529a8c193bf780d9748d5e0a26b6f42343d
artugatuga <148588412+artugatuga@users.noreply.github.com>
2024-10-24 18:02:19 +0100

    comecei a mutant Tree

---
+++ Assets/Scripts/Enemies/MutantTrees/Attack/MutantEnemieAttack.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutantEnemieAttack : MonoBehaviour
+{
+    [Header("Detect Player Settings")]
+    [SerializeField] private CircleCollider2D coll;
+    [SerializeField] private float radius;
+    [SerializeField] private bool attack;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        coll.radius = radius;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+        }
+    }
+
+}
