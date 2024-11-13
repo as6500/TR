41d4dc35c18eba4bbdb7b6981e8795c8b8669854
Maffittaa <mafaldamartins2001@gmail.com>
2024-11-13 15:43:15 +0000

    Location quests completly done

--- Assets/Scripts/QuestSystem/QuestTypeLocation.cs
+++ Assets/Scripts/QuestSystem/QuestTypeLocation.cs
@@ -17,7 +17,6 @@ public class QuestTypeLocation : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             onLocation = true;
-            Destroy(gameObject);
         }
     }
 
