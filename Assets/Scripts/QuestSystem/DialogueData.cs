3330daa792954ae6f6dbb9497f6a9d4d2c2a94d9
Maffittaa <mafaldamartins2001@gmail.com>
2024-12-14 09:56:36 +0000

    dialogues added, starrting manager

--- Assets/Scripts/QuestSystem/DialogueData.cs
+++ Assets/Scripts/QuestSystem/DialogueData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "ScriptableObjects/DialogueData")] 
-public class DialogData : ScriptableObject
+public class DialogueData : ScriptableObject
 {
     public List<String> dialogue;
 }
--- Assets/Scripts/QuestSystem/DialogueManager.cs
+++ Assets/Scripts/QuestSystem/DialogueManager.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public DialogueData activeDialogue;
+    public QuestManager questManager;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (questManager.activeQuestState == QuestState.Pending)
+        {
+            
+        }
     }
 }
