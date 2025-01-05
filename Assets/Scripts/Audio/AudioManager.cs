82a86ba610f3fb9fb9fe48f988a7ca34290ef6e1
artugatuga <148588412+artugatuga@users.noreply.github.com>
2025-01-05 13:30:19 +0000

    AudioManager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Music")]
+    [SerializeField] private AudioSource MainMenuMusic;
+    [SerializeField] private AudioSource GamePlayMusic;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Player")]
+    [Header("   Movement")]
+    [SerializeField] private AudioSource stepOneSolidFloor;
+    [SerializeField] private AudioSource stepTwoSolidFloor;
+    [SerializeField] private AudioSource stepOneSandFloor;
+    [SerializeField] private AudioSource stepTwoSandFloor;
+    
+    [Header("   Weapons")]
+    [SerializeField] private AudioSource shoot;
+    [SerializeField] private AudioSource swingOne;
+    [SerializeField] private AudioSource swingTwo;
+
+    [Header("   Health")]
+    [SerializeField] private AudioSource loseHealthOne;
+    [SerializeField] private AudioSource loseHealthTwo;
+    [SerializeField] private AudioSource loseHealthThree;
+    [SerializeField] private AudioSource gainHealth;
+
+    [Header("   FlashLight")]
+    [SerializeField] private AudioSource flashlightOn;
+    [SerializeField] private AudioSource flashlightOff;
+
+    [Header("Enemies")]
+    [Header("   MutantTree")]
+    [SerializeField] private AudioSource sapOne;
+    [SerializeField] private AudioSource sapTwo;
+    [SerializeField] private AudioSource sapThree;
+
+    [Header("   Worm")]
+    [SerializeField] private AudioSource drag;
 }
